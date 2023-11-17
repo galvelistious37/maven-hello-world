8d813b4c2be610cbeda3278315dd549a30c1692f
johnny <john.robertson88@yahoo.com>
2023-11-17 00:01:02 -0800

    Initial commit

---
+++ src/main/java/com/johnny/Main.java
@@ -0,0 +1,10 @@
+package com.johnny;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println(StringUtils.capitalize("hello world"));
+    }
+}
